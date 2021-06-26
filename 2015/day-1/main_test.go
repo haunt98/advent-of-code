cef879ef9562be63f958425aa4eb1f0e9911dbe4
Hau Nguyen <hauvipapro@gmail.com>
2021-06-26 10:06:45 +0000

    test: 2015/day-1 part 2

--- 2015/day-1/main_test.go
+++ 2015/day-1/main_test.go
@@ -61,3 +61,32 @@ func TestGoFloor(t *testing.T) {
 		})
 	}
 }
+
+func TestGoIndexFloorUntil(t *testing.T) {
+	tests := []struct {
+		name      string
+		s         string
+		stopFloor int
+		want      int
+	}{
+		{
+			name:      ")",
+			s:         ")",
+			stopFloor: -1,
+			want:      1,
+		},
+		{
+			name:      "()())",
+			s:         "()())",
+			stopFloor: -1,
+			want:      5,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := goIndexFloorUntil(tc.s, tc.stopFloor)
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
